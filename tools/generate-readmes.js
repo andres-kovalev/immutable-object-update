7ada431658d6823c8a3b936a84a42cd00cf72cc1
Andres Kovalev <mail@akovalev.ru>
2019-10-30 17:09:13 +0100

    refactor: refactored generate-readmes.js helper a bit

--- tools/generate-readmes.js
+++ tools/generate-readmes.js
@@ -5,13 +5,16 @@ jsdoc2md.getTemplateDataSync({ files: './src/*/*/index.js' })
     .forEach(generateReadme);
 
 function generateReadme(meta) {
-    const { filename, path } = meta.meta;
-    const [ name ] = filename.split('.');
-    const mdName = name === 'index' ? 'README' : name;
+    const file = getMarkdownFileName(meta);
+    const markdown = generateReadmeMarkdown(meta);
 
+    writeFile(file, markdown);
+}
+
+function generateReadmeMarkdown(meta) {
     const { scope, customTags, ...data } = meta;
 
-    let md = jsdoc2md.renderSync({
+    const markdown = jsdoc2md.renderSync({
         data: [ data ],
         'param-list-format': 'list'
     });
@@ -21,8 +24,16 @@ function generateReadme(meta) {
             ({ tag }) => tag === 'docs'
         ) || {};
 
-        md = [ md, '**Description**', value ].join('\n\n');
+        return [ markdown, '**Description**', value ].join('\n\n');
     }
 
-    writeFile(`${ path }/${ mdName }.md`, md);
+    return markdown;
+}
+
+function getMarkdownFileName({ meta }) {
+    const { filename, path } = meta;
+    const [ name ] = filename.split('.');
+    const mdName = name === 'index' ? 'README' : name;
+
+    return `${ path }/${ mdName }.md`;
 }
