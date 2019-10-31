13ede77672482533329ce84ebfe45c958ee61612
Andres Kovalev <mail@akovalev.ru>
2019-11-01 00:03:03 +0100

    test: extracted tests for operations commons into separate module

--- tests/set.spec.js
+++ tests/set.spec.js
@@ -13,58 +13,10 @@ describe('set', () => {
         'b.b1': 5
     };
 
-    context('when array-type path provided', () => {
-        it('should update value', () => {
-            const newValue = 6;
+    it('should set provided value', () => {
+        const updated = set(source, 'a.a1', 10);
 
-            const result = set(source, [ 'b', 'b1' ], newValue);
-
-            expect(result.b.b1).to.be.equal(newValue);
-        });
-
-        it('should update value not considering dots as delimiter', () => {
-            const newValue = 6;
-
-            const result = set(source, [ 'b.b1' ], newValue);
-
-            expect(result['b.b1']).to.be.equal(newValue);
-        });
-    });
-
-    context('when string-type path provided', () => {
-        const newValue = 6;
-
-        let result;
-        beforeEach(() => {
-            result = set(source, 'b.b1', newValue);
-        });
-
-        it('should update value considering dots as delimiter', () => {
-            expect(result.b.b1).to.be.equal(newValue);
-            expect(result['b.b1']).to.be.equal(source['b.b1']);
-        });
-
-        it('should keep same refs for unchanged values', () => {
-            expect(result.a).to.be.equal(source.a);
-            expect(result.a.a1).to.be.equal(source.a.a1);
-            expect(result.a.a2).to.be.equal(source.a.a2);
-            expect(result.b.b2).to.be.equal(source.b.b2);
-            expect(result['b.b1']).to.be.equal(source['b.b1']);
-        });
-
-        it('should update refs for changed values', () => {
-            expect(result).to.not.be.equal(source);
-            expect(result.b).to.not.be.equal(source.b);
-            expect(result.b.b1).to.not.be.equal(source.b.b1);
-        });
-    });
-
-    it('should ignore empty path items', () => {
-        const newValue = 6;
-
-        const result = set(source, 'b..b1', newValue);
-
-        expect(result.b.b1).to.be.equal(newValue);
+        expect(updated.a.a1).to.be.equal(10);
     });
 
     it('should create intermediate items if not exists', () => {
@@ -73,15 +25,6 @@ describe('set', () => {
         const result = set(source, 'c.c1', newValue);
 
         expect(result.c.c1).to.be.equal(newValue);
-    });
-
-    it('should create arrays item if not exists and addressed by index', () => {
-        const newValue = 6;
-
-        const result = set(source, 'c.0', newValue);
-
-        expect(result.c).to.be.an('array');
-        expect(result.c[0]).to.be.equal(newValue);
     });
 
     it('should support partial application', () => {
--- tests/update.spec.js
+++ tests/update.spec.js
@@ -15,63 +15,19 @@ describe('update', () => {
         'b.b1': 5
     };
 
-    context('when array-type path provided', () => {
-        it('should update value', () => {
-            const result = update(source, [ 'b', 'b1' ], increment);
-
-            expect(result.b.b1).to.be.equal(4);
-        });
-
-        it('should update value not considering dots as delimiter', () => {
-            const result = update(source, [ 'b.b1' ], increment);
-
-            expect(result['b.b1']).to.be.equal(6);
-        });
-    });
-
     context('when string-type path provided', () => {
-        let result;
-        beforeEach(() => {
-            result = update(source, 'b.b1', increment);
-        });
-
         it('should update value considering dots as delimiter', () => {
+            const result = update(source, 'b.b1', increment);
+
             expect(result.b.b1).to.be.equal(4);
             expect(result['b.b1']).to.be.equal(source['b.b1']);
         });
-
-        it('should keep same refs for unchanged values', () => {
-            expect(result.a).to.be.equal(source.a);
-            expect(result.a.a1).to.be.equal(source.a.a1);
-            expect(result.a.a2).to.be.equal(source.a.a2);
-            expect(result.b.b2).to.be.equal(source.b.b2);
-            expect(result['b.b1']).to.be.equal(source['b.b1']);
-        });
-
-        it('should update refs for changed values', () => {
-            expect(result).to.not.be.equal(source);
-            expect(result.b).to.not.be.equal(source.b);
-            expect(result.b.b1).to.not.be.equal(source.b.b1);
-        });
-    });
-
-    it('should ignore empty path items', () => {
-        const result = update(source, 'b..b1', increment);
-
-        expect(result.b.b1).to.be.equal(4);
     });
 
     it('should create intermediate items if not exists', () => {
         const result = update(source, 'c.c1', increment);
 
         expect(result.c.c1).to.be.equal(1);
-    });
-
-    it('should create arrays item if not exists and addressed by index', () => {
-        const result = update(source, 'c.0', increment);
-
-        expect(result.c).to.be.an('array');
-        expect(result.c[0]).to.be.equal(1);
     });
 
     it('should support partial application', () => {
